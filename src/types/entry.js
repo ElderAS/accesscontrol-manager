fb3f447b00a8f71f57a7ad8f80c2191ca8c031b9
Carsten Jacobsen <car.jacobsen@gmail.com>
2019-02-26 15:33:49 +0100

    fix(Entry): fixed bug where isOwner was set wrong

--- src/types/entry.js
+++ src/types/entry.js
@@ -1,8 +1,11 @@
-function Entry(data, options = {}) {
-  Entry.prototype.isOwner = options.isOwner
-  Object.assign(this, data)
-
-  return this
+class Entry {
+  constructor(data, options = {}) {
+    Object.assign(this, data);
+    Object.defineProperty(this, "isOwner", {
+      value: options.isOwner,
+      writable: false
+    });
+  }
 }
 
-module.exports = Entry
+module.exports = Entry;
--- test/types.entry.test.js
+++ test/types.entry.test.js
@@ -1,14 +1,20 @@
-const Entry = require('../src/types/entry')
-const MovieMock = require('./mocks/entries/movie')
+const Entry = require("../src/types/entry");
+const MovieMock = require("./mocks/entries/movie");
 
-test('Entry returns same output as input data', () => {
-  expect(new Entry(MovieMock, { isOwner: false })).toEqual(MovieMock)
-})
+test("Entry returns same output as input data", () => {
+  expect(new Entry(MovieMock, { isOwner: false })).toEqual(MovieMock);
+});
 
-test('Entry isOwner is true', () => {
-  expect(new Entry(MovieMock, { isOwner: true }).isOwner).toBe(true)
-})
+test("Entry isOwner is true", () => {
+  let entry1 = new Entry(MovieMock, { isOwner: true });
+  let entry2 = new Entry(MovieMock, { isOwner: false });
+  let entry3 = new Entry(MovieMock, { isOwner: true });
 
-test('Entry isOwner is false', () => {
-  expect(new Entry(MovieMock, { isOwner: false }).isOwner).toBe(false)
-})
+  expect(entry1.isOwner).toBe(true);
+  expect(entry2.isOwner).toBe(false);
+  expect(entry3.isOwner).toBe(true);
+});
+
+test("Entry isOwner is false", () => {
+  expect(new Entry(MovieMock, { isOwner: false }).isOwner).toBe(false);
+});
